cbb927e126e2f09b756c90b2f7144137819ce73e
Patrick Louis <patricklouisw@gmail.com>
2020-11-26 20:45:51 -0500

    updated library

---
+++ script.js
@@ -0,0 +1,15 @@
+import { mainContainer, flipCardHover, flipCardClick, bookAnimation } from './lib.js';
+
+mainContainer();
+
+flipCardHover({
+    // width: 250,
+    // height: 320
+});
+
+flipCardClick({
+    // width: 250,
+    // height: 320
+});
+
+bookAnimation({})
